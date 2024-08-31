ef64a161eada4619127a3b69697e0048947aa197
Jenziner <nik@jenzer.ch>
2024-08-31 10:41:50 +0200

    fixes #2 - uuid / pwd handling

--- WALLET/src/app/app.component.ts
+++ WALLET/src/app/app.component.ts
@@ -1,16 +1,20 @@
 import { Component } from '@angular/core';
-import { IonApp, IonRouterOutlet } from '@ionic/angular/standalone';
+import { CommonModule } from '@angular/common';
+import { IonicModule } from '@ionic/angular';
 import { MenuComponent } from './menu/menu.component';
 import { TabSendingPage } from './tab_sending/tab_sending.page';
-
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   standalone: true,
-  imports: [IonApp, IonRouterOutlet,MenuComponent,TabSendingPage],
+  imports: [
+    CommonModule,
+    IonicModule,
+    MenuComponent,
+    TabSendingPage
+  ],
 })
-
 
 export class AppComponent {
   constructor() {}
--- WALLET/src/app/menu/menu.component.ts
+++ WALLET/src/app/menu/menu.component.ts
@@ -1,17 +1,78 @@
 import { Component, OnInit } from '@angular/core';
-import { IonicModule } from '@ionic/angular';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { IonicModule } from '@ionic/angular'; 
+import { Storage } from '@ionic/storage-angular';
+import { v4 as uuidv4 } from 'uuid';
 
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
   styleUrls: ['./menu.component.scss'],
   standalone: true,
-  imports: [IonicModule],
+  imports: [
+    CommonModule,
+    IonicModule,
+    FormsModule,
+  ],
+  providers: [Storage],
 })
+
 export class MenuComponent  implements OnInit {
+  uuid: string | null = null;
+  password: string | null = null;
+  isGenerated: boolean = false;
+  isImportMode: boolean = false;
 
-  constructor() { }
+  constructor(private storage: Storage) { 
+    this.init();
+  }
 
-  ngOnInit() {}
+  async init() {
+    await this.storage.create();
+  }
+
+  async ngOnInit() {
+    this.uuid = await this.storage.get('uuid');
+    this.password = await this.storage.get('password');
+    this.isGenerated = !!this.uuid;
+  }
+
+  async generateCredentials() {
+    if (!this.isGenerated) {
+      this.uuid = uuidv4();
+      this.password = this.generatePassword();
+      await this.storage.set('uuid', this.uuid);
+      await this.storage.set('password', this.password);
+      this.isGenerated = true;
+    }
+  }
+
+  generatePassword(): string {
+    // Simple generator, to be changed later
+    return Math.random().toString(36).slice(-8);
+  }
+
+  enableImportMode() {
+    this.isImportMode = true;
+  }
+
+  async saveImportedData() {
+    if (this.uuid && this.password) {
+      await this.storage.set('uuid', this.uuid);
+      await this.storage.set('password', this.password);
+      this.isGenerated = true;
+      this.isImportMode = false;
+    }
+  }
+
+  async deleteData() {
+    await this.storage.remove('uuid');
+    await this.storage.remove('password');
+    this.uuid = null;
+    this.password = null;
+    this.isGenerated = false;
+  }
 
 }
+
