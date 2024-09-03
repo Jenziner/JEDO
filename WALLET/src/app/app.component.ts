3ea44805e19db689b418ea0267ca238f79f20d9d
Jenziner <nik@jenzer.ch>
2024-09-03 14:45:26 +0200

    fixes #18 - icon and splashscreen added

--- WALLET/src/app/app.component.ts
+++ WALLET/src/app/app.component.ts
@@ -4,6 +4,8 @@ import { IonicModule } from '@ionic/angular';
 import { Storage } from '@ionic/storage-angular';
 import { MenuComponent } from './menu/menu.component';
 import { TabSendingPage } from './tab_sending/tab_sending.page';
+import { SplashScreen } from '@capacitor/splash-screen';
+
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
@@ -26,6 +28,11 @@ export class AppComponent {
 
   async initializeApp() {
     await this.storage.create(); 
+
+    await SplashScreen.show({
+      showDuration: 2000,
+      autoHide: true,
+    });
   } 
 
 }
