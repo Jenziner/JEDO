003f8c34ef10abf455c60aee0df736dd432ae2dc
Jenziner <nik@jenzer.ch>
2025-12-13 12:49:42 +0100

    GIT cleanup

---
+++ jedo.dev/test/debug.sh
@@ -0,0 +1,34 @@
+WHITEB='\033[1;29m'
+WHITE='\033[0;29m'
+BLACKB='\033[1;30m'
+BLACK='\033[0;30m'
+REDB='\033[1;31m'
+RED='\033[0;31m'
+GREENB='\033[1;32m'
+GREEN='\033[0;32m'
+YELLOWB='\033[1;33m'
+YELLOW='\033[0;33m'
+BLUEB='\033[1;34m'
+BLUE='\033[0;34m'
+PURPLEB='\033[1;35m'
+PURPLE='\033[0;35m'
+TURQUOISEB='\033[1;36m'
+TURQUOISE='\033[0;36m'
+NC='\033[0m' # No Color
+
+
+echo -e "${REDB}Roter Titel ${NC}"
+echo -e "${RED}Roter Text ${NC}"
+echo -e "${YELLOWB}Gelber Titel ${NC}"
+echo -e "${YELLOW}Gelber Text ${NC}"
+echo -e "${GREENB}Grüner Titel ${NC}"
+echo -e "${GREEN}Grüner Text ${NC}"
+echo -e "${BLUEB}Blauer Titel ${NC}"
+echo -e "${BLUE}Blauer Text ${NC}"
+
+
+
+echo -e "${TEST1}Test1 Text ${NC}"
+echo -e "${TEST2}Test2 Text ${NC}"
+echo -e "${TEST3}Test3 Text ${NC}"
+echo -e "${TEST4}Test4 Text ${NC}"
